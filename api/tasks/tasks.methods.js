a5db703b8aedf48ef9e38cc82884e94e6325f811
Gabriel Grubba <gabriel@discourse.org>
2024-12-10 22:00:43 -0300

    DEV: add slow queries

--- api/tasks/tasks.methods.js
+++ api/tasks/tasks.methods.js
@@ -1,7 +1,119 @@
-import { check } from 'meteor/check';
-import { Meteor } from 'meteor/meteor';
-import { checkLoggedIn, checkTaskOwner } from '../lib/auth';
-import { Tasks } from './tasks';
+import { check } from "meteor/check";
+import { Meteor } from "meteor/meteor";
+import { checkLoggedIn, checkTaskOwner } from "../lib/auth";
+import { Tasks } from "./tasks";
+
+/**
+ *
+ * @param {"slow"|"normal"|"super slow"} kind
+ * @returns
+ */
+const slowQueryString = (kind) => {
+  const kindTable = {
+    normal: 10,
+    slow: 100,
+    "super slow": 1000,
+  };
+  return `const gigantMatrix = new Array(${kindTable[kind]})
+  .fill(0)
+  .map(() => new Array(${10 * kindTable[kind]}).fill(0));
+function doubleEveryArrayElement(arr) {
+  return arr.map((x) => x * 2);
+}
+
+function randomizeArray(arr) {
+  return arr.map((x) => Math.random());
+}
+
+function sortSlowlyArray(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+gigantMatrix.forEach((gigantArray) => {
+  sortSlowlyArray(
+    randomizeArray(
+      doubleEveryArrayElement(sortSlowlyArray(randomizeArray(gigantArray)))
+    )
+  );
+});`;
+};
+
+/**
+ *
+ * @param {string} name task name
+ * @param {"fast"|"slow"|"normal"|"super slow"} kind
+ * @returns {void}
+ */
+const complexCalculationOrLookup = async (name, kind) => {
+  const kindTable = {
+    fast: {
+      query: async () =>
+        Tasks.find({
+          $where: `!this.done`,
+        }).fetchAsync(),
+    },
+    normal: {
+      query: async () =>
+        Tasks.find({
+          $where: `
+          ${slowQueryString(kind)}
+          return this.done
+          `,
+        }).fetchAsync(),
+    },
+    slow: {
+      query: async () =>
+        Tasks.find({
+          $and: [
+            {
+              $where: `
+              ${slowQueryString(kind)}
+              return this.done;
+              `,
+            },
+            {
+              $where: `
+              ${slowQueryString(kind)}
+              return this.description.includes("o");
+              `,
+            },
+            {
+              $and: [
+                {
+                  $where: `
+                  ${slowQueryString(kind)}
+                  return this.done;
+                  `,
+                },
+                {
+                  $where: `
+                  ${slowQueryString(kind)}
+                  return this.description.includes("i");
+                  `,
+                },
+              ],
+            },
+          ],
+        }).fetchAsync(),
+    },
+    "super slow": {
+      query: async () =>
+        Tasks.find({
+          $where: `
+          ${slowQueryString(kind)}
+return (this.done && (this.description.includes("o") || this.description.includes("a")));
+          `,
+        }).fetchAsync(),
+    },
+  };
+  console.log(`Starting ${name} at ${new Date()}`);
+  const docs = await kindTable[kind].query();
+  console.log(`Found ${docs.length} documents`);
+  console.log({
+    docs,
+  });
+  console.log(`Ending ${name} at ${new Date()}`);
+};
 
 /**
 
@@ -21,6 +133,7 @@ async function insertTask({ description }) {
     userId: Meteor.userId(),
     createdAt: new Date(),
   };
+  await complexCalculationOrLookup("sending email", "slow");
   return Tasks.insertAsync(task);
 }
 
@@ -35,6 +148,10 @@ async function insertTask({ description }) {
 async function removeTask({ taskId }) {
   check(taskId, String);
   await checkTaskOwner({ taskId });
+  await complexCalculationOrLookup(
+    "checking for task references and lookups",
+    "normal"
+  );
   return Tasks.removeAsync(taskId);
 }
 
@@ -50,7 +167,12 @@ async function toggleTaskDone({ taskId }) {
   check(taskId, String);
   await checkTaskOwner({ taskId });
   const task = await Tasks.findOneAsync(taskId);
+  await complexCalculationOrLookup("sending notifications", "super slow");
   return Tasks.updateAsync({ _id: taskId }, { $set: { done: !task.done } });
 }
 
-Meteor.methods({ insertTask, removeTask, toggleTaskDone });
+Meteor.methods({
+  insertTask,
+  removeTask,
+  toggleTaskDone,
+});
--- ui/pages/tasks/hooks/use-tasks.jsx
+++ ui/pages/tasks/hooks/use-tasks.jsx
@@ -1,10 +1,11 @@
-import { useUserId } from 'meteor/react-meteor-accounts';
-import { useFind, useSubscribe } from 'meteor/react-meteor-data/suspense';
-import { useState } from 'react';
-import { Tasks } from '/api/tasks/tasks';
+import { useUserId } from "meteor/react-meteor-accounts";
+import { useFind, useSubscribe } from "meteor/react-meteor-data/suspense";
+import { useEffect, useState } from "react";
+import { Tasks } from "/api/tasks/tasks";
+import { Meteor } from "meteor/meteor";
 
 export function useTasks() {
-  useSubscribe('tasksByLoggedUser');
+  useSubscribe("tasksByLoggedUser");
   const userId = useUserId();
   const [hideDone, setHideDone] = useState(false);
   const filter = hideDone ? { done: { $ne: true }, userId } : { userId };
